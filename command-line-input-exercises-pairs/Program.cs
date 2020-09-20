b8a0daaabc946f401a693918b92d58f1ee8941e6
Joe <jhowell957@gmail.com>
2020-09-20 03:53:44 -0400

    went a little crazy with the ones we did. but didnt touch the binary challenge

--- command-line-input-exercises-pairs/Program.cs
+++ command-line-input-exercises-pairs/Program.cs
@@ -31,3 +31,44 @@ namespace command_line_input_exercises_pairs
         }
     }
 }
+/*
+ * 
+ * 
+ * 
+ * 
+ * Console.WriteLine("Welcome to the Martian weight Converter.");       //start here 
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            char cont = 'J';
+                do
+                {
+                    Console.WriteLine("Please enter your weight on Earth:( More than one if neccessary, just put a space between each one)");
+                    string eW = Console.ReadLine();
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    char[] delimiters = { ' ' };
+                    string[] weighArray = eW.Split(delimiters,StringSplitOptions.RemoveEmptyEntries);
+                    for (int i = 0; i < weighArray.Length; i++)
+                    {
+                        decimal oriW = decimal.Parse(weighArray[i]);
+                        decimal mW = oriW * 0.378M;
+                        Console.WriteLine($" Earth weight is {oriW} lbs. | Martian weight is {mW} lbs.");
+                    }
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" Did you want to enter any  more weights?");
+                    cont = Char.Parse(Console.ReadLine());
+                    if (cont != 'y' || cont != 'Y' || cont != 'n' || cont != 'N') 
+                    {
+                        Console.WriteLine(" (Y) for yes or (N) for no.");
+                        cont = char.Parse(Console.ReadLine());
+                    }
+                    
+
+
+                } 
+            while ((cont == 'y') || (cont == 'Y'));
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Thats all folks. Thanks for using our Converter. Have a fun time on Mars! Bon Voyage!!!!");
+*/
